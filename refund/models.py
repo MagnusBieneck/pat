73da5d9d3bb304f15d40fb210a944c8d096c9f4f
Magnus Bieneck <magnus@bieneck.eu>
2019-06-06 18:08:44 +0200

    Create Refund and Receipt models #2

--- refund/models.py
+++ refund/models.py
@@ -1,4 +1,37 @@
 """Module containing models for the refund app."""
-# from django.db import models
+from django.db import models
 
-# Create your models here.
+REFUND_TYPES = [
+    ("cash", "Bargeld"),
+    ("bank_account", "Überweisung"),
+    ("amazon", "Amazon-Account"),
+    ("credit_card", "Kreditkarte")
+]
+
+
+class Refund(models.Model):
+    """Model representing an expense refund form."""
+    date_submitted = models.DateField()
+    department_leader = models.TextField()
+    account = models.TextField()
+    cost_centre = models.TextField()
+    project = models.TextField(blank=True, null=True)
+
+    refund_type = models.TextField(choices=REFUND_TYPES)
+    bank_account_owner = models.TextField(blank=True, null=True)
+    bank_account_iban = models.TextField(blank=True, null=True)
+    bank_account_bic = models.TextField(blank=True, null=True)
+
+    @property
+    def amount(self):
+        """Return the sum of all receipts belonging to this refund."""
+        # pylint: disable=no-member
+        return float(sum([receipt.amount for receipt in Receipt.objects.filter(refund=self)]))
+
+
+class Receipt(models.Model):
+    """Model representing a receipt."""
+    refund = models.ForeignKey("Refund", on_delete=models.CASCADE)
+    amount = models.DecimalField(decimal_places=2, max_digits=8)
+
+    # Add document field here!
---
+++ tests/unit/refund/test_models.py
@@ -0,0 +1,33 @@
+"""Module containing tests for the refund models."""
+import pytest
+from refund.models import Receipt
+
+
+@pytest.mark.django_db
+def test_refund_basic(refund, refund_dict):
+    """Test basic functionality of the Refund model."""
+    refund.save()
+
+    assert refund.date_submitted == refund_dict["date_submitted"]
+    assert refund.department_leader == refund_dict["department_leader"]
+    assert refund.account == refund_dict["account"]
+    assert refund.cost_centre == refund_dict["cost_centre"]
+    assert refund.project == refund_dict["project"]
+    assert refund.refund_type == refund_dict["refund_type"]
+    assert refund.bank_account_owner == refund_dict["bank_account_owner"]
+    assert refund.bank_account_iban == refund_dict["bank_account_iban"]
+    assert refund.bank_account_bic == refund_dict["bank_account_bic"]
+
+
+@pytest.mark.django_db
+def test_refund_amount(refund):
+    """Test that the sum of a refund is computed correctly."""
+    refund.save()
+
+    receipt_one = Receipt(refund=refund, amount=10.7)
+    receipt_one.save()
+
+    receipt_two = Receipt(refund=refund, amount=13.2)
+    receipt_two.save()
+
+    assert refund.amount == 23.9
