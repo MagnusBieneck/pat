7fdea42158e5459e4a211866e8b25ee46f8b9f83
Magnus Bieneck <magnus@bieneck.eu>
2019-06-08 19:02:51 +0200

    Create basic refund request form #2

--- refund/admin.py
+++ refund/admin.py
@@ -1,4 +1,5 @@
 """Module where the models are registered for admin panel."""
-# from django.contrib import admin
+from django.contrib import admin
+from refund.models import Refund
 
-# Register your models here.
+admin.site.register(Refund)
--- refund/views.py
+++ refund/views.py
@@ -1,4 +1,31 @@
 """Module containing views for the Refund app."""
-# from django.shortcuts import render
+from datetime import date
+from django.http import HttpResponseRedirect
+from django.shortcuts import render
 
-# Create your views here.
+from refund.forms import RefundForm
+
+
+def request_form(request):
+    """The request form view."""
+
+    if request.method == "POST":
+
+        form = RefundForm(request.POST)
+        if form.is_valid():
+
+            form.instance.date_submitted = date.today()
+            form.save()
+
+            return HttpResponseRedirect("/form-submitted")
+
+    else:
+
+        form = RefundForm()
+
+    return render(request, "refund/request_form.html", {"form": form, "title": "Expense refund"})
+
+
+def form_submitted(request):
+    """The view shown after the form has been submitted."""
+    return render(request, "refund/form_submitted.html", {"title": "Form submitted"})
