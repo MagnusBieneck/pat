c447d7fd464e5815848dc26beb2dca3ea8e42715
Magnus Bieneck <magnus@bieneck.eu>
2019-06-16 22:58:08 +0200

    Create test that checks for untranslated parts #15

---
+++ tests/test_translation.py
@@ -0,0 +1,17 @@
+"""Module containing tests for the translation files."""
+import os
+import pytest
+
+
+@pytest.mark.parametrize("language_code", ['de'])
+def test_untranslated_parts(language_code):
+    """Test that there are no untranslated parts in the *.po files."""
+    locale_dir = "locale/{}/LC_MESSAGES".format(language_code)
+
+    for file_name in os.listdir(locale_dir):
+        if file_name.endswith(".po"):
+
+            with open("{}/{}".format(locale_dir, file_name)) as file:
+
+                # THere is always one occurrence of an empty msgstr in the beginning of a .po file.
+                assert file.read().count('msgstr ""') == 1
